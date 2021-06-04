4796b4a24e986676dd1541132cbc59b78ae39bed
sethpoly <emailspolyniak@gmail.com>
2021-06-04 12:09:44 -0400

    Changed homedefault wording

--- src/App.js
+++ src/App.js
@@ -4,10 +4,6 @@ import './App.css';
 import Login from './components/Login';
 import Home from './components/Home';
 
-import CreateBook from './components/CreateBook';
-import ShowBookList from './components/ShowBookList';
-import ShowBookDetails from './components/ShowBookDetails';
-import UpdateBookInfo from './components/UpdateBookInfo';
 
 class App extends Component {
   render() {
@@ -16,9 +12,6 @@ class App extends Component {
         <div>
           <Route exact path='/' component={Home} />
           <Route path="/login" component={Login} />
-
-          <Route path='/edit-book/:id' component={UpdateBookInfo} />
-          <Route path='/show-book/:id' component={ShowBookDetails} />
         </div>
       </Router>
     );
--- src/components/HomeDefault.js
+++ src/components/HomeDefault.js
@@ -7,10 +7,10 @@ function HomeDefault() {
   return (
     <div>
       <div className="home-header-container">
-          <h1 className="header-title">What's on your shelf?</h1>
+          <h1 className="header-title">Callbacks, interviews, rejections.</h1>
           <h2 className="subheader-title"><span 
               style={{color:'var(--red-color)'}}>[Insert Name]. </span> 
-              A simple video-game tracker.</h2>
+              A job application tracker.</h2>
           <Link to="/Login">
             <button className="btn-signup">SIGN UP</button>
           </Link>
